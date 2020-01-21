178da98cffbb51c556daa8c87da48a5a9ec8f66b
Mateus Vilione <mateusvilione@gmail.com>
2020-01-20 22:55:40 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const mongoose = require('../configs/mongoose_connection');
+const cors = require('cors');
 const routes = require('./routes');
 const port = process.env.PORT;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
